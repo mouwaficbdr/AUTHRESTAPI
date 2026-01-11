fdb82332c9d290bd54cbbef8b6945b1549d8f3ef
mouwaficbdr <badaroumouwafic@gmail.com>
2026-01-11 22:47:12 +0100

    Migration de l'authentification 2FA vers la nouvelle API otplib

--- src/lib/authenticator.js
+++ src/lib/authenticator.js
@@ -1,18 +1,19 @@
-import { authenticator } from 'otplib';
+import { generateSecret, verify, generateURI } from 'otplib';
 
-// Pour que lors de la vérification de code à 6 chiffres, on accorde 30 sec en avant et en arrière pour le calcul en fonction du temps.
-authenticator.options = { window: 1 };
-
-export const generateSecret = async () => {
-    return authenticator.generateSecret();
-}
+export const generatedSecret = async () => {
+  return generateSecret();
+};
 
 export const verifyCode = (token, secret) => {
-    return authenticator.check(token, secret);
-}
+  return verify({ token, secret });
+};
 
 export const generateQRCode = async (email, secret) => {
-    const uri = authenticator.keyuri(email, 'ACTIVATION_2FA', secret);
+  const uri = generateURI({
+    issuer: 'ACTIVATION_2FA',
+    label: email,
+    secret,
+  });
 
-    return await qrcode.toDataURL(uri);
-}
+  return await qrcode.toDataURL(uri);
+};
